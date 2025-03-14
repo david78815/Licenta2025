3c15cb83cb1d9bf819ed727c8d3cc93e9d727471
Postolea David <postoleadavid22@stud.ase.ro>
2025-03-14 04:10:36 +0200

    Add files via upload

---
+++ AppL/src/main/java/com/example/appl/LevelModel.java
@@ -0,0 +1,29 @@
+package com.example.appl;
+
+public class LevelModel {
+    private final String levelName;
+    private final String description;
+    private final int score;
+    private final String completionTime;
+    private final boolean locked;
+
+    public LevelModel(String levelName, String description, int score, String completionTime, boolean locked) {
+        this.levelName = levelName;
+        this.description = description;
+        this.score = score;
+        this.completionTime = completionTime;
+        this.locked = locked;
+    }
+
+    public LevelModel(String levelName, String description){
+        this(levelName, description, 0 , "--:--",true);
+    }
+
+    // Getters
+    public String getLevelName() { return levelName; }
+    public String getDescription() { return description; }
+    public int getScore() { return score; }
+    public String getCompletionTime() { return completionTime; }
+    public boolean isLocked() { return locked; }
+}
+
---
+++ AppL/src/main/java/com/example/appl/MainController.java
@@ -0,0 +1,33 @@
+package com.example.appl;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainController {
+    @FXML
+    private Button lvlSelBtn;
+
+    @FXML
+    private void onLvlSelBtnClick(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("lvlSelect-view.fxml"));
+            Parent lvlSelectRoot = loader.load();
+
+            Scene lvlSelectScene = new Scene(lvlSelectRoot);
+
+            Stage stage = (Stage) lvlSelBtn.getScene().getWindow();
+
+            stage.setScene(lvlSelectScene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
